06ea79ee4b9dc7875c9eb0bf199dd67897da01df
Theodor <ftri2001@gmail.com>
2023-05-23 13:32:01 +0300

    Added LAF/Compcov test target

--- src/fuzzy_goat/fuzzy_goat.cc
+++ src/fuzzy_goat/fuzzy_goat.cc
@@ -1,12 +1,19 @@
-// Axel '0vercl0k' Souchet - July 10 2021
+// Theodor Arsenij 'm4drat' - May 23 2023
+
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <Windows.h>
 #include <array>
 #include <cstdio>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vcruntime_string.h>
 
-void FuzzMe(const char *buffer, uint32_t size) {
+uint32_t FuzzingCoverageFeedbackTest(const char *buffer, uint32_t size) {
   if (size < 10) {
-    return;
+    return 0;
   }
 
   if (buffer[0] == 'F') {
@@ -17,8 +24,7 @@ void FuzzMe(const char *buffer, uint32_t size) {
             if (buffer[5] == 'N') {
               if (buffer[6] == 'G') {
                 if (buffer[7] == '!') {
-                  uint32_t data = *(uint32_t *)nullptr;
-                  printf("%d", data);
+                  abort();
                 }
               }
             }
@@ -27,15 +33,87 @@ void FuzzMe(const char *buffer, uint32_t size) {
       }
     }
   }
+
+  return 1;
 }
 
-int main() {
+uint32_t FuzzingCompcovLafTest(const char *buffer, uint32_t size) {
+  if (size < 32) {
+    return 0;
+  }
+
+  // Check if we can solve 8-byte comparison
+  if (*(uint64_t *)buffer != 0x13375612DEADBEEF) {
+    return 1;
+  }
+  buffer += sizeof(uint64_t);
+
+  // Check if we can solve 4-byte comparison
+  if (*(uint32_t *)buffer != 0xDEADBEEF) {
+    return 2;
+  }
+  buffer += sizeof(uint32_t);
+
+  // Check if we can solve 2-byte comparison
+  if (*(uint16_t *)buffer != 0x1337) {
+    return 3;
+  }
+  buffer += sizeof(uint16_t);
+
+  // strcmp
+  const char str1[] = "Never gonna give you up";
+  // Here, and below we're calling these functions using volatile function
+  // pointers. This way, the compiler should emit a library-call instead of
+  // inlining the whole function.
+  int (*volatile strcmp_ptr)(const char *_Str1, const char *_Str2) = strcmp;
+  if (strcmp_ptr(buffer, str1) != 0) {
+    return 4;
+  }
+  buffer += sizeof(str1);
+
+  // strncmp
+  const char str2[] = "Never gonna run around and desert you";
+  int (*volatile strncmp_ptr)(const char *_Str1, const char *_Str2,
+                              size_t _MaxCount) = strncmp;
+  if (strncmp_ptr(buffer, str2, sizeof(str2)) != 0) {
+    return 5;
+  }
+  buffer += sizeof(str2);
+
+  // memcmp
+  const char str3[] = "We've known each other for so long";
+  int (*volatile memcmp_ptr)(const void *_Buf1, const void *_Buf2,
+                             size_t _Size) = memcmp;
+  if (memcmp_ptr(buffer, str3, sizeof(str3)) != 0) {
+    return 6;
+  }
+  buffer += sizeof(str3);
+
+  // TODO: compare instructions, such as: cmps, scasb, ...
+
+  abort();
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Usage: %s <test-mode> (compcov-laf, bb-coverage)\n", argv[0]);
+    return 1;
+  }
+
   std::array<uint8_t, 1024> Buffer;
   if (getenv("BREAK") != nullptr) {
     __debugbreak();
   }
 
-  FuzzMe((const char *)Buffer.data(), Buffer.size());
+  if (strcmp(argv[1], "compcov-laf") == 0) {
+    return FuzzingCoverageFeedbackTest((const char *)Buffer.data(),
+                                       Buffer.size());
+  } else if (strcmp(argv[1], "bb-coverage") == 0) {
+    return FuzzingCompcovLafTest((const char *)Buffer.data(), Buffer.size());
+  } else {
+    printf("Usage: %s <test-mode> (compcov-laf, bb-coverage)\n", argv[0]);
+    return 1;
+  }
 
   return EXIT_SUCCESS;
 }
