e721070d9ff11ff92ec1a8b4ba0f82e618f72b4e
Theodor <ftri2001@gmail.com>
2023-07-01 21:42:26 +0300

    Analyze experiments script - improved

--- scripts/analyze-experiments.py
+++ scripts/analyze-experiments.py
@@ -1,12 +1,10 @@
-from scipy.stats import norm
-
-import seaborn as sns
-import matplotlib.pyplot as plt
 import argparse as ap
-import pandas as pd
 import pathlib
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 import yaml
-import json
 
 
 def load_single_result(result_file: pathlib.Path) -> pd.DataFrame:
@@ -28,9 +26,7 @@ def load_single_result(result_file: pathlib.Path) -> pd.DataFrame:
     return dataframe
 
 
-def load_results(
-    results_dir: pathlib.Path, base: str, alternative: str
-) -> pd.DataFrame:
+def load_results(results_dir: pathlib.Path) -> pd.DataFrame:
     dataframe = pd.DataFrame()
     for result in results_dir.glob("*.json"):
         result_df = load_single_result(result)
@@ -47,17 +43,11 @@ def load_experiment_config(exp_config: pathlib.Path) -> tuple:
             print(exc)
             exit(1)
 
-    results_dir = pathlib.Path(
-        config.get("results-dir", "experiment-results")
-    ).resolve()
+    results_dir = pathlib.Path(config.get("results-dir", "experiment-results")).resolve()
     base_config = pathlib.Path(config.get("base-config", None)).stem
     alternative_config = pathlib.Path(config.get("alternative-config", None)).stem
 
     return (results_dir, base_config, alternative_config)
-
-
-def do_stats(results: pd.DataFrame, base: str, alternative: str):
-    pass
 
 
 def plot_results(results: pd.DataFrame, base: str, alternative: str):
@@ -66,7 +56,7 @@ def plot_results(results: pd.DataFrame, base: str, alternative: str):
     IGNORE_FIRST_N = 10
     IGNORE_LAST_N = -22
 
-    for graph_type in ('coverage', 'execs_sec', 'corpus_size'):
+    for graph_type in ("coverage", "execs_sec", "corpus_size"):
         base_mean = results.iloc[
             IGNORE_FIRST_N:IGNORE_LAST_N,
             results.columns.str.contains(f"{graph_type}-bb-coverage-.*-{base}"),
@@ -85,20 +75,55 @@ def plot_results(results: pd.DataFrame, base: str, alternative: str):
             results.columns.str.contains(f"{graph_type}-bb-coverage-.*-{alternative}"),
         ].std(axis=1)
 
-        plt.figure() # dpi=300
-        ax = sns.lineplot(
-            data=pd.DataFrame({"no-laf": base_mean, "laf": alt_mean}),
+        start_time = results.iloc[
+            IGNORE_FIRST_N,
+            results.columns.str.contains("relative_timestamp-bb-coverage-.*-.*"),
+        ].mean()
+        end_time = results.iloc[
+            IGNORE_LAST_N,
+            results.columns.str.contains("relative_timestamp-bb-coverage-.*-.*"),
+        ].mean()
+
+        plt.figure(dpi=150)
+        ax = plt.subplot()
+        ax.xaxis.set_major_formatter(mpl.dates.DateFormatter("%H:%M"))
+        ax.xaxis.set_major_locator(mpl.dates.MinuteLocator(interval=30))
+        ax.set_xlabel("time")
+        ax.set_xlim(
+            pd.to_datetime(start_time - 400, unit="s"), pd.to_datetime(end_time + 400, unit="s")
         )
+        plt.xticks(rotation=45)
+
+        timestamps = pd.date_range(
+            start=pd.to_datetime(start_time, unit="s"),
+            end=pd.to_datetime(end_time, unit="s"),
+            periods=len(base_mean),
+        )
+
+        ax.plot(
+            timestamps,
+            base_mean,
+            label="no-laf",
+            linewidth=2,
+        )
+        ax.plot(
+            timestamps,
+            alt_mean,
+            label="laf",
+            linestyle="--",
+            linewidth=2,
+        )
+        ax.legend()
         ax.set(title=graph_type)
 
         ax.fill_between(
-            results.index[IGNORE_FIRST_N:IGNORE_LAST_N],
+            timestamps,
             base_mean - base_std,
             base_mean + base_std,
             alpha=0.2,
         )
         ax.fill_between(
-            results.index[IGNORE_FIRST_N:IGNORE_LAST_N],
+            timestamps,
             alt_mean - alt_std,
             alt_mean + alt_std,
             alpha=0.2,
@@ -116,9 +141,7 @@ def plot_results(results: pd.DataFrame, base: str, alternative: str):
                 ],
                 "laf": results.loc[
                     COV_ENTRY_N,
-                    results.columns.str.contains(
-                        f"coverage-bb-coverage-.*-{alternative}"
-                    ),
+                    results.columns.str.contains(f"coverage-bb-coverage-.*-{alternative}"),
                 ],
             }
         ),
@@ -130,18 +153,30 @@ def plot_results(results: pd.DataFrame, base: str, alternative: str):
 def main():
     parser = ap.ArgumentParser()
     parser.add_argument(
-        "exp_config",
+        "-b",
+        "--base",
+        type=str,
+        help="Name of the base configuration (e.g. no-laf)",
+    )
+    parser.add_argument(
+        "-a",
+        "--alternative",
+        type=str,
+        help="Name of the alternative configuration (e.g. laf)",
+    )
+    parser.add_argument(
+        "results",
         type=pathlib.Path,
-        help="Path to the experiment configuration file",
+        help="Path to the results directory",
     )
     args = parser.parse_args()
 
-    results_dir, base_config, alternative_config = load_experiment_config(
-        args.exp_config
-    )
-    results = load_results(results_dir, base_config, alternative_config)
-    do_stats(results, base_config, alternative_config)
-    plot_results(results, base_config, alternative_config)
+    base_name = args.base
+    alternative_name = args.alternative
+    results_dir = args.results
+
+    results = load_results(results_dir, base_name, alternative_name)
+    plot_results(results, base_name, alternative_name)
 
 
 if __name__ == "__main__":
